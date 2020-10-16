9c8db0bb6d99ab6ae70b0ffa9ceba653e175ecc3
CStank89 <C.Stank89@gmail.com>
2020-10-16 13:08:28 -0400

    finished

---
+++ client/src/utils/testFetch.js
@@ -0,0 +1,10 @@
+import { axiosWithAuth } from "./axiosWithAuth";
+
+export const testFetch = () => {
+  axiosWithAuth()
+    .get("/api/colors")
+    .then((res) => {
+      console.log(res);
+      return res;
+    });
+};
